a65f94ff266013ec63ca43994843da6564df889d
Justin Shaffer <justinbshaffer91@gmail.com>
2023-05-16 15:53:23 -0700

    Implement VendorsController

---
+++ VendorOrderTracker/Controllers/VendorController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Vendor selectedVendor = Vendor.Find(id);
+      return View(selectedVendor);
+    }
+
+    [HttpGet("/vendors/{id}/orders/new")]
+    public ActionResult CreateOrder(int id)
+    {
+      Vendor selectedVendor = Vendor.Find(id);
+      return View(selectedVendor);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult CreateOrder(int vendorId, string orderTitle, string orderDescription, int orderPrice, string orderDate)
+    {
+      Vendor selectedVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      selectedVendor.AddOrder(newOrder);
+      return RedirectToAction("Show", new { id = vendorId });
+    }
+  }
+}
