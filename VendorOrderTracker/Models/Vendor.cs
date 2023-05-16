0490f98e1226c7d5e2a39a65647f36565e71b464
Justin Shaffer <justinbshaffer91@gmail.com>
2023-05-16 15:33:27 -0700

    Create Vendor class with necessary methods

---
+++ VendorOrderTracker/Models/Vendor.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Vendor
+  {
+    public int Id { get; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Order> Orders { get; set; }
+    private static List<Vendor> _instances = new List<Vendor>();
+
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      Orders = new List<Order>();
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances.Find(vendor => vendor.Id == searchId);
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
+
+    public static void DeleteVendor(int vendorId)
+    {
+      _instances.RemoveAll(vendor => vendor.Id == vendorId);
+    }
+
+    public void DeleteAllOrders()
+    {
+      Orders.Clear();
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public void UpdateVendor(string newName, string newDescription)
+    {
+      Name = newName;
+      Description = newDescription;
+    }
+  }
+}
